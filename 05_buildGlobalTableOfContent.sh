1cf101a020098f6c06d0ae19920e58667720afae
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2026-03-05 11:21:36 +1100

    tidy

--- 05_buildGlobalTableOfContent.sh
+++ 05_buildGlobalTableOfContent.sh
@@ -1,21 +1,8 @@
 #!/usr/bin/env bash
-# 05_buildGlobalIndex.sh - Rebuild the global index
+# 05_buildGlobalIndex.sh - Rebuild the global table of content
 
 set -euo pipefail
 
 echo "Building global table of content..."
 python3 scripts/buildGlobalTableOfContent.py
 echo "Done. Global table of content updated"
-
-# echo "Building search index..."
-# python3 scripts/search-index.py build
-# echo "Done. Index updated at scripts/search_index.json"
-
-# gzip -f scripts/search_index.json
-# echo "Done. Gzipped scripts/search_index.json to scripts/search_index.json.gz"
-
-# mv -f scripts/search_index.json.gz docs/
-# # rm -f scripts/search_index.json.gz
-# echo "Done. scripts/search_index.json.gz moved to docs/search_index.json.gz"
-
-# ls -alrh docs/search_index.json.gz
